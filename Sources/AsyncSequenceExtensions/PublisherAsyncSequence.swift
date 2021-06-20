d715b7eb532364b4d2f5b2e8b7d7b3fea876e63a
Joseph Lord <joseph@human-friendly.com>
2021-06-21 00:05:01 +0100

    First untested (but compiling) draft of Combine -> AsyncSequence - Publisher.asynSequence

---
+++ Sources/AsyncSequenceExtensions/PublisherAsyncSequence.swift
@@ -0,0 +1,82 @@
+
+import Combine
+import _Concurrency
+import Metal
+
+@available(macOS 12.0, *)
+struct PublisherAsyncSequence<Element> : AsyncSequence {
+
+    let publisher: AnyPublisher<Element, Error>
+
+    func makeAsyncIterator() -> Iterator {
+        let itr = Iterator()
+        publisher.receive(subscriber: itr)
+        return itr
+        
+    }
+
+    actor Iterator : AsyncIteratorProtocol, Subscriber {
+        typealias Input = Element
+        typealias Failure = Error
+        
+        private var subscription: Subscription?
+
+        private var continuation: CheckedContinuation<Element?, Error>?
+
+        func next() async throws -> Element? {
+            try await withCheckedThrowingContinuation({ continuation in
+                self.continuation = continuation
+                subscription?.request(.max(1))
+            })
+        }
+
+        nonisolated func receive(subscription: Subscription) {
+            async {
+                await self.receive(sub: subscription)
+            }
+        }
+        
+        private func receive(sub: Subscription) async {
+            self.subscription = sub
+        }
+        
+        nonisolated func receive(completion: Subscribers.Completion<Error>) {
+            async {
+                await receive(compl: completion)
+            }
+        }
+        private func receive(compl: Subscribers.Completion<Error>) async {
+            assert(continuation != nil)
+            switch compl {
+            case .finished:
+                continuation?.resume(returning: nil)
+            case .failure(let err):
+                continuation?.resume(throwing: err)
+            }
+            continuation = nil
+        }
+        
+        nonisolated func receive(_ input: Element) -> Subscribers.Demand {
+            async {
+                await receive(input: input)
+            }
+            return .none
+        }
+        private func receive(input: Element) {
+            assert(continuation != nil)
+            continuation?.resume(returning: input)
+            continuation = nil
+            
+        }
+    }
+
+}
+
+
+@available(macOS 12.0, *)
+extension Publisher where Self.Failure == Error {
+    var asyncSequence: PublisherAsyncSequence<Output> {
+        PublisherAsyncSequence(publisher: self.eraseToAnyPublisher())
+    }
+}
+

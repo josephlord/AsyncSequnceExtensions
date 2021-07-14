233f760fee07cdde20fbcb3d2cc65170dc6eaee7
Joseph Lord <git@jtl.me.uk>
2021-07-14 17:18:38 +0100

    Minimal test case for actor continuation hanging.

---
+++ Tests/AsyncSequenceExtensionsTests/ActorContinuationTests.swift
@@ -0,0 +1,47 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joseph Lord on 10/07/2021.
+//
+
+import XCTest
+
+@available(iOS 15.0, macOS 12.0, *)
+actor SUTActor {
+    var continuation: CheckedContinuation<(),Never>?
+    var canGo = false
+    
+    func pause() async {
+        if canGo { return }
+        await withCheckedContinuation { (continuation: CheckedContinuation<Void, Never>) -> Void in
+//            if canGo {
+//                continuation.resume(returning: ())
+//            } else {
+                self.continuation = continuation
+//            }
+        }
+    }
+    
+    func go() {
+        canGo = true
+        continuation?.resume()
+    }
+}
+
+@available(iOS 15.0, macOS 12.0, *)
+final class ActorContinuationTests : XCTestCase {
+    func testPauseGo() {
+        let sut = SUTActor()
+        let exp = expectation(description: "Will resume")
+        Task.detached(priority: .high) {
+            await sut.pause()
+            exp.fulfill()
+        }
+        Task.detached(priority: .default) {
+            await sut.go()
+        }
+        waitForExpectations(timeout: 0.2, handler: nil)
+        _ = sut // Ensure lifetime sufficient
+    }
+}
